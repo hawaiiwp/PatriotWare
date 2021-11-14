9e1d40cee40bd71ea6a05804ab9b840c84400ce2
Logan <32753924+hufflegamer123@users.noreply.github.com>
2021-11-13 23:14:44 -0500

    Update findFilesFunc.sh

--- functions/findFilesFunc.sh
+++ functions/findFilesFunc.sh
@@ -3,15 +3,4 @@
 #this was made to shorten the individual_scripts/deleteFiles.sh script. 
 #input 1 is the file extension, input 2 is the output file, and input 3 should be 'first' if it is the first search, and 'after' if it isn't.
 
-
-echo "Would you like to delete $1 files? (y/n)"
-read -p 'y/n: ' RESP
-if [ $RESP == 'y' ]
-then
-    echo
-    echo 'Searching for files'
-    find / -iname "*$1"
-    echo 'Found files'
-else
-    echo 'Moving on'
-fi
+find / -iname "*$1"
